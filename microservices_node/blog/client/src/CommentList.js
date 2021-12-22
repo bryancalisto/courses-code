e83671c9e3df378904e88c0363d671660e6e59bb
Bryan Calisto <bryancalisto@yahoo.es>
2021-12-21 21:14:29 -0500

    New: Added frontend app

---
+++ microservices_node/blog/client/src/CommentList.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const CommentList = ({ comments }) => {
+  const renderComments = Object.values(comments).map(comment => {
+    let content;
+
+    console.log(comment);
+    if (comment.status === 'approved') {
+      content = comment.content
+    }
+
+    if (comment.status === 'pending') {
+      content = 'This comment is awaiting moderation';
+    }
+
+    if (comment.status === 'rejected') {
+      content = 'This comment has been rejected';
+    }
+
+    return <li key={comment.id}>{content}</li>
+  });
+
+  return <ul>{renderComments}</ul>;
+}
+
+export default CommentList;
