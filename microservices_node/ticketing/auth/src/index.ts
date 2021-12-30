b0c868acad8af0eb1e26bba41fb97376b1d82fe1
Bryan Calisto <bryancalisto@yahoo.es>
2021-12-29 20:07:09 -0500

    New: Added route files

--- microservices_node/ticketing/auth/src/index.ts
+++ microservices_node/ticketing/auth/src/index.ts
@@ -1,13 +1,18 @@
 import express from 'express';
 
+import { currentUserRouter } from './routes/current-user';
+import { signinRouter } from './routes/signin';
+import { signoutRouter } from './routes/signout';
+import { signupRouter } from './routes/signup';
+
 const app = express();
 app.use(express.json());
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
 
 const port = 3000;
-
-app.get('/api/users/currentUser', (req, res) => {
-  res.send('hoLA');
-});
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
