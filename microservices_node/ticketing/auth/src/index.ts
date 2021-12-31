3754db0a96b6aece0218b1c3e719baabbab2f172
Bryan Calisto <bryancalisto@yahoo.es>
2021-12-30 19:49:48 -0500

    New: Added error handling mechanisms

--- microservices_node/ticketing/auth/src/index.ts
+++ microservices_node/ticketing/auth/src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import { errorHandler } from './middlewares/error-handler';
 
 import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
@@ -11,6 +12,7 @@ app.use(currentUserRouter);
 app.use(signinRouter);
 app.use(signoutRouter);
 app.use(signupRouter);
+app.use(errorHandler);
 
 const port = 3000;
 
