c3e8a763681c32b35607a8356b05c6c648162015
Bryan Calisto <bryancalisto@yahoo.es>
2022-01-03 09:51:10 -0500

    New: Added signup mechanisms

--- microservices_node/ticketing/auth/src/index.ts
+++ microservices_node/ticketing/auth/src/index.ts
@@ -3,6 +3,7 @@ import 'express-async-errors';
 import { NotFoundError } from './errors/not-found-error';
 import { errorHandler } from './middlewares/error-handler';
 import mongoose from 'mongoose';
+import cookieSession from 'cookie-session';
 
 import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
@@ -10,7 +11,13 @@ import { signoutRouter } from './routes/signout';
 import { signupRouter } from './routes/signup';
 
 const app = express();
+app.set('trust proxy', true);
 app.use(express.json());
+app.use(cookieSession({
+  signed: false,
+  secure: true
+}));
+
 app.use(currentUserRouter);
 app.use(signinRouter);
 app.use(signoutRouter);
@@ -25,6 +32,11 @@ app.use(errorHandler);
 const port = 3000;
 
 const start = async () => {
+  // Check env variable existence
+  if (!process.env.JWT_KEY) {
+    throw new Error('JWT_KEY must be defined');
+  }
+
   try {
     await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
     console.log('Connected to DB');
--- microservices_node/ticketing/auth/src/routes/signup.ts
+++ microservices_node/ticketing/auth/src/routes/signup.ts
@@ -1,14 +1,17 @@
 import express, { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
+import { BadRequestError } from '../errors/bad-request-error';
 import { RequestValidationError } from '../errors/request-validation.error';
-import { DatabaseConnectionError } from '../errors/database-connection-error';
+import { User } from '../models/user';
+import jwt from 'jsonwebtoken';
+
 
 const router = express.Router();
 
 router.post('/api/users/signup', [
   body('email').isEmail().withMessage('Email must be valid'),
   body('password').trim().isLength({ min: 4, max: 20 }).withMessage('Password must be between 4 and 20 chars'),
-], (req: Request, res: Response) => {
+], async (req: Request, res: Response) => {
   const errors = validationResult(req);
 
   if (!errors.isEmpty()) {
@@ -16,9 +19,26 @@ router.post('/api/users/signup', [
   }
 
   const { email, password } = req.body;
-  console.log('creating a user');
-  throw new DatabaseConnectionError();
-  res.send({});
+
+  const existingUser = await User.findOne({ email });
+
+  if (existingUser) {
+    throw new BadRequestError('Email in use');
+  }
+
+  const user = User.build({ email, password });
+  await user.save();
+
+  const userJwt = jwt.sign({
+    id: user._id,
+    email: user.email
+  }, process.env.JWT_KEY!);
+
+  req.session = {
+    jwt: userJwt
+  };
+
+  res.status(201).send(user);
 });
 
 export { router as signupRouter };
