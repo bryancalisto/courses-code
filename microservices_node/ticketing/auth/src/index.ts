c6c790d73ad46e8d80ec6b95eecdbfd7c272f151
Bryan Calisto <bryancalisto@yahoo.es>
2022-01-05 21:30:03 -0500

    Refactor: Moved express app to its own file

--- microservices_node/ticketing/auth/src/index.ts
+++ microservices_node/ticketing/auth/src/index.ts
@@ -1,35 +1,5 @@
-import express from 'express';
-import 'express-async-errors';
-import { NotFoundError } from './errors/not-found-error';
-import { errorHandler } from './middlewares/error-handler';
 import mongoose from 'mongoose';
-import cookieSession from 'cookie-session';
-
-import { currentUserRouter } from './routes/current-user';
-import { signinRouter } from './routes/signin';
-import { signoutRouter } from './routes/signout';
-import { signupRouter } from './routes/signup';
-
-const app = express();
-app.set('trust proxy', true);
-app.use(express.json());
-app.use(cookieSession({
-  signed: false,
-  secure: true
-}));
-
-app.use(currentUserRouter);
-app.use(signinRouter);
-app.use(signoutRouter);
-app.use(signupRouter);
-
-app.all('*', async () => {
-  throw new NotFoundError();
-})
-
-app.use(errorHandler);
-
-const port = 3000;
+import { app } from './app';
 
 const start = async () => {
   // Check env variable existence
@@ -43,6 +13,8 @@ const start = async () => {
   } catch (e) {
     console.error(e);
   }
+
+  const port = 3000;
 
   app.listen(port, () => {
     console.log(`Listening on port ${port}`);
