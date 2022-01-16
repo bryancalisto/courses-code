2727744eae751d7f99528bbd046ea9b90942002d
Bryan Calisto <bryancalisto@yahoo.es>
2022-01-16 13:09:50 -0500

    Chore: Added funtionality to signup page

--- microservices_node/ticketing/client/pages/index.js
+++ microservices_node/ticketing/client/pages/index.js
@@ -1,3 +1,16 @@
-export default () => {
-  return <h1>Index</h1>;
+import axios from "axios";
+
+const LandingPage = ({ color }) => {
+  console.log('I am on the component', color);
+  return <h1>Landing Page</h1>;
 }
+
+LandingPage.getInitialProps = async () => {
+  try {
+    const response = await axios.get('http://ingress-nginx.ingress-nginx-controller.svc.cluster.local/api/users/currentuser')
+    return response.data;
+  } catch (err) {
+    return null;
+  }
+}
+export default LandingPage;
