9f23ff45ff01ea97b2aecd4ea0a4286dc2e8e709
Bryan Calisto <bryancalisto@yahoo.es>
2022-01-23 12:51:42 -0500

    New: Added ticket model

--- microservices_node/ticketing/tickets/src/routes/__test__/new.test.ts
+++ microservices_node/ticketing/tickets/src/routes/__test__/new.test.ts
@@ -1,5 +1,6 @@
 import request from "supertest";
 import { app } from "../../app";
+import { Ticket } from "../../models/ticket";
 
 it('has a route handler listening to /api/tickets for post requests', async () => {
   const resp = await request(app)
@@ -63,15 +64,26 @@ it('returns an error if an invalid price is provided', async () => {
     .expect(400);
 });
 
-// it('Creates a ticket with valid inputs', async () => {
-//   // Add in a check to make sure a ticket was saved
+it('Creates a ticket with valid inputs', async () => {
+  // Add in a check to make sure a ticket was saved
+  let tickets = await Ticket.find({});
+  expect(tickets.length).toEqual(0);
 
-//   await request(app)
-//     .post('/api/tickets')
-//     .set('Cookie', global.signin())
-//     .send({
-//       title: 'asdffjdkla',
-//       price: 20
-//     })
-//     .expect(201);
-// });
+  const title = 'asdffjdkla';
+  const price = 20
+
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+      title,
+      price
+    })
+    .expect(201);
+
+
+  tickets = await Ticket.find({});
+  expect(tickets.length).toEqual(1);
+  expect(tickets[0].title).toEqual(title);
+  expect(tickets[0].price).toEqual(price);
+});
--- microservices_node/ticketing/tickets/src/routes/new.ts
+++ microservices_node/ticketing/tickets/src/routes/new.ts
@@ -1,14 +1,29 @@
 import express, { Response, Request } from 'express';
 import { body } from 'express-validator';
 import { requireAuth, validateRequest } from '@bcticketing/common';
+import { Ticket } from '../models/ticket';
 
 const router = express.Router();
 
 router.post('/api/tickets', [
   body('title').not().isEmpty().withMessage('Title is required'),
   body('price').isFloat({ gt: 0 }).withMessage('Price must be greater than zero'),
-], requireAuth, validateRequest, async (req: Request, res: Response) => {
-  return res.sendStatus(200);
-});
+], requireAuth, validateRequest,
+  async (req: Request, res: Response) => {
+    const { title, price } = req.body;
+
+    try {
+      const newTicket = Ticket.build({
+        title,
+        price,
+        userId: req.currentUser!.id
+      });
+
+      await newTicket.save();
+
+      res.status(201).send(newTicket.toJSON());
+    } catch (e) {
+    }
+  });
 
 export { router as createTicketRouter };
