d1f3cad8571d7aef92be24f4f73868383968aa1a
Bryan Calisto <bryancalisto@yahoo.es>
2022-02-20 21:48:07 -0500

    New: Added nats related stuff for testing

--- microservices_node/ticketing/nats-test/src/publisher.ts
+++ microservices_node/ticketing/nats-test/src/publisher.ts
@@ -1,19 +1,22 @@
 import nats from 'node-nats-streaming';
+import { TicketCreatedPublisher } from './events/ticket-created-publisher';
 
 const stan = nats.connect('test-cluster', 'abc', {
   url: 'http://localhost:4222'
 });
 
-stan.on('connect', () => {
+stan.on('connect', async () => {
   console.log('Publisher connected to NATS');
 
-  const data = JSON.stringify({
+  const publisher = new TicketCreatedPublisher(stan);
+
+  await publisher.publish({
     id: '123',
     title: 'concert',
     price: 20
   });
 
-  stan.publish('ticket:created', data, () => {
-    console.log('Event published');
-  });
+  // stan.publish('ticket:created', data, () => {
+  //   console.log('Event published');
+  // });
 });
