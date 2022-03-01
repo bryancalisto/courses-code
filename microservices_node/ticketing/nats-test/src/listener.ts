6aa4b0d12ac5a15c0ba0c0a64d345c0cc2d8e370
Bryan Calisto <bryancalisto@yahoo.es>
2022-02-28 23:50:32 -0500

    Refactor: Updated NATS connetion string to support port forwarding

--- microservices_node/ticketing/nats-test/src/listener.ts
+++ microservices_node/ticketing/nats-test/src/listener.ts
@@ -2,7 +2,7 @@ import nats from 'node-nats-streaming';
 import { randomBytes } from 'crypto';
 import { TicketCreatedListener } from './events/ticket-created-listener';
 
-const stan = nats.connect('test-cluster', randomBytes(4).toString('hex'), {
+const stan = nats.connect('ticketing', randomBytes(4).toString('hex'), {
   url: 'http://localhost:4222'
 });
 
