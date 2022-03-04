5ded45a864c25f013cd0b66cfee1acfeaddf5b02
Bryan Calisto <bryancalisto@yahoo.es>
2022-03-04 00:34:30 -0500

    New: Added test for nats event for update route

--- microservices_node/ticketing/tickets/src/routes/__test__/update.test.ts
+++ microservices_node/ticketing/tickets/src/routes/__test__/update.test.ts
@@ -1,6 +1,7 @@
 import request from 'supertest';
 import { app } from '../../app';
 import mongoose from 'mongoose';
+import { natsWrapper } from '../../nats-wrapper';
 
 it('Returns 404 if the provided id does not exist', async () => {
   const id = new mongoose.Types.ObjectId().toHexString();
@@ -121,4 +122,31 @@ it('Updates a ticket provided of valid input', async () => {
 
   expect(ticketResp.body.title).toEqual(newTitle);
   expect(ticketResp.body.price).toEqual(newPrice);
+});
+
+it('Generates update event', async () => {
+  const cookie = global.signin();
+
+  const response = await request(app)
+    .post(`/api/tickets`)
+    .set('Cookie', cookie)
+    .send({
+      title: 'title',
+      price: 10
+    });
+
+  const newTitle = 'newTitle';
+  const newPrice = 1000;
+
+  await request(app)
+    .put(`/api/tickets/${response.body.id}`)
+    .set('Cookie', cookie)
+    .send({
+      title: newTitle,
+      price: newPrice
+    })
+    .expect(200);
+
+
+  expect(natsWrapper.client.publish).toHaveBeenCalled();
 });
