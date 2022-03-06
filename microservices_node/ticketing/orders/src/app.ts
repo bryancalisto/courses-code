c55c4f611aaa24c0356a6ae8ac5fb107e41d33ba
Bryan Calisto <bryancalisto@yahoo.es>
2022-03-06 13:28:24 -0500

    New: Added models and route templates

--- microservices_node/ticketing/orders/src/app.ts
+++ microservices_node/ticketing/orders/src/app.ts
@@ -3,10 +3,10 @@ import 'express-async-errors';
 import { NotFoundError, errorHandler, currentUser } from '@bcticketing/common';
 import cookieSession from 'cookie-session';
 
-import { createTicketRouter } from './routes/new';
-import { showTicketRouter } from './routes/show';
-import { indexTicketRouter } from './routes/index';
-import { updateTicketRouter } from './routes/update';
+import { newOrderRouter } from './routes/new';
+import { showOrderRouter } from './routes/show';
+import { indexOrderRouter } from './routes/index';
+import { deleteOrderRouter } from './routes/delete';
 
 
 const app = express();
@@ -19,10 +19,10 @@ app.use(cookieSession({
 
 app.use(currentUser);
 
-app.use(createTicketRouter);
-app.use(showTicketRouter);
-app.use(indexTicketRouter);
-app.use(updateTicketRouter);
+app.use(newOrderRouter);
+app.use(showOrderRouter);
+app.use(indexOrderRouter);
+app.use(deleteOrderRouter);
 
 app.all('*', async () => {
   throw new NotFoundError();
