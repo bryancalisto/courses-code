4f87f9fae39db269ce24c188f4fc6b4712202515
Bryan Calisto <bryancalisto@yahoo.es>
2022-06-03 18:24:08 -0500

    New: Tests

--- eth/kickstart/test/campaing.test.js
+++ eth/kickstart/test/campaing.test.js
@@ -33,5 +33,18 @@ describe('Campaigns', () => {
   it('deploy a factory and a campaign', () => {
     assert.ok(factory.options.address);
     assert.ok(campaign.options.address);
-  })
+  });
+
+  it('marks caller as the campaign manager', async () => {
+    const manager = await campaign.methods.manager().call();
+    assert(manager, accounts[0]);
+  });
+
+  it('allows people to contribute money to a campaign and includes them as approvers', async () => {
+    // A person contributes
+    await campaign.methods.contribute().send({ from: accounts[1], value: '101' });
+    // Validate that person was marked as approver
+    const isContributor = await campaign.methods.approvers(accounts[1]).call();
+    assert.ok(isContributor);
+  });
 });
