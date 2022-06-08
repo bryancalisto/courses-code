3c21983b0a9d81bab05116e7bb40deff4d7917f7
Bryan Calisto <bryancalisto@yahoo.es>
2022-06-07 23:53:45 -0500

    Refactor: Updated connection to window's web3 object

--- eth/kickstart/ethereum/web3.js
+++ eth/kickstart/ethereum/web3.js
@@ -4,7 +4,7 @@ let web3;
 
 if (typeof window !== "undefined" && typeof window.ethereum !== "undefined") {
   // We are in the browser and metamask is running.
-  web3 = new Web3(window.web3.currentProvider);
+  web3 = new Web3(window.ethereum);
 } else {
   // We are on the server or the user is not running metamask
   const provider = new Web3.providers.HttpProvider('https://rinkeby.infura.io/v3/efee60774c844b508bdd1c895e9ab5a3');
