931f863d1486b65fb003345c014d491c63a2cf80
Bryan Calisto <bryancalisto@yahoo.es>
2022-08-16 22:12:21 -0500

    Debugger was born

--- crafting_interpreters/clox/src/main.c
+++ crafting_interpreters/clox/src/main.c
@@ -6,6 +6,7 @@ int main(int argc, char *argv[])
   Chunk chunk;
   initChunk(&chunk);
   writeChunk(&chunk, OP_RETURN);
+  disassembleChunk(&chunk, "Test chunk");
   freeChunk(&chunk);
 
   return 0;
