d670a0e48f27b30d3041f1f9bc1e8ee872b45873
Bryan Calisto <bryancalisto@yahoo.es>
2022-08-31 19:18:01 -0500

    Added line information to chunk

--- crafting_interpreters/clox/include/chunk.h
+++ crafting_interpreters/clox/include/chunk.h
@@ -16,11 +16,12 @@ typedef struct
   int capacity;
   ValueArray constants;
   uint8_t *code;
+  int *lines;
 } Chunk;
 
 void initChunk(Chunk *chunk);
 void freeChunk(Chunk *chunk);
-void writeChunk(Chunk *chunk, uint8_t byte);
+void writeChunk(Chunk *chunk, uint8_t byte, int line);
 int addConstant(Chunk *chunk, Value constant);
 
 #endif
