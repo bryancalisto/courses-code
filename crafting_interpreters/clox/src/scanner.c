5c8b1e741dad5c1cf59d7fe065b2132834c998dd
Bryan Calisto <bryancalisto@yahoo.es>
2022-09-29 22:07:28 -0500

    Added identifier detection

--- crafting_interpreters/clox/src/scanner.c
+++ crafting_interpreters/clox/src/scanner.c
@@ -20,6 +20,13 @@ void initScanner(const char *source)
   scanner.line = 1;
 }
 
+static bool isAlpha(char c)
+{
+  return (c >= 'a' && c <= 'z') ||
+         (c >= 'A' && c <= 'Z') ||
+         c == '_';
+}
+
 static bool isDigit(char c)
 {
   return c >= '0' && c <= '9';
@@ -165,6 +172,21 @@ static void skiWhitespace()
   }
 }
 
+TokenType identifierType()
+{
+  return TOKEN_IDENTIFIER;
+}
+
+static Token identifier()
+{
+  while (isAlpha(peek()) || isDigit(peek))
+  {
+    advance();
+  }
+
+  return makeToken(identifierType());
+}
+
 Token scanToken()
 {
   skiWhitespace();
@@ -176,6 +198,11 @@ Token scanToken()
   }
 
   char c = advance();
+
+  if (isAlpha(c))
+  {
+    return identifier();
+  }
 
   if (isDigit(c))
   {
